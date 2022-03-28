e35939d52c2a91bdf1ae56f84604d0c709fae49f
ericwrobinson <erobs16x@yahoo.com>
2022-03-28 13:07:07 -0400

    sup

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -17,7 +17,7 @@ export default class PortfolioIndexPage extends React.Component {
     const 
       subheading = 'Work',
       title = 'Magic Pie works.',
-      subtitle = 'Magic Pie Copywriting means communicating, collaborating, and creating. How do we do it? Usually with a conversation over a cup of coffee or a couple cold ones. Then we work together to identify needs, chalk up a plan of action, and execute shared strategies. The result is rock-solid copy for websites, videos, blogs, social media, and more.';
+      subtitle = 'Magic Pie Copywriting means communicating, collaborating, and creating. How do we do it? Usually with a conversation over a cup of coffee or a couple cold ones. Then we work together to identify needs, chalk up a plan of action, and execute shared strategies. The result is rock-solid copy for websites, videos, blogs, and more.';
     return (
       <PageTransition>
         <Layout>
